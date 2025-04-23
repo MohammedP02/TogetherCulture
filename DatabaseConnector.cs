f08771b2c62ddbf2feab9688368759dee57a0821
MohammedP02 <MZKP100@studen.aru.ac.uk>
2025-04-23 15:42:37 +0300

    Created the User and Database Connector Classes

---
+++ DatabaseConnector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace TogetherCulture
+{
+    internal class DatabaseConnector
+    {
+        var dbCon = DBConnection.Instance();
+        dbCon.Server = "YourServer";
+dbCon.DatabaseName = "YourDatabase";
+dbCon.UserName = "YourUsername";
+dbCon.Password = "YourPassword";
+if (dbCon.IsConnect())
+    }
+}
---
+++ User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TogetherCulture
+{
+    internal class User
+    {
+        // User class variables with auto-implemented getter and setter methods
+        public int userID { get; private set; }
+        public String username { get; private set; }
+        public String password { get; private set; }
+        public String role { get; private set; }
+
+        // Constructor to initialize the 
+        public User(int userID,String username,String password,String role) {
+            this.userID = userID;
+            this.username = username;
+            this.password = password;
+            this.role = role;
+        }
+
+        public void login() { 
+        
+        
+        }
+
+        
+
+
+    }
+}
